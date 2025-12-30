85eec90b68fd1bcc0165363b26e646f6da8d5343
ICROSStheLINE <coolwillrule123@gmail.com>
2025-12-29 19:29:27 -0500

    - Added basic ahh movement

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -1,16 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float turnSpeed = 180f;
+
+    [Header("Camera")]
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float mouseSensitivity = 2f;
+    [SerializeField] float minPitch = -35f;
+    [SerializeField] float maxPitch = 65f;
+
+    float pitch;
+
     void Start()
     {
-        
+        if (cameraTransform == null && Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleLook();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 moveInput = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) { moveInput += Vector3.forward; }
+        if (Input.GetKey(KeyCode.S)) { moveInput += Vector3.back; }
+        if (Input.GetKey(KeyCode.A)) { moveInput += Vector3.left; }
+        if (Input.GetKey(KeyCode.D)) { moveInput += Vector3.right; }
+
+        if (moveInput.sqrMagnitude > 0f)
+        {
+            moveInput.Normalize();
+            Vector3 worldMove = transform.TransformDirection(moveInput) * moveSpeed * Time.deltaTime;
+            transform.position += worldMove;
+        }
+    }
+
+    void HandleLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        transform.Rotate(Vector3.up, mouseX * turnSpeed * Time.deltaTime);
+
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+
+        if (cameraTransform != null)
+        {
+            Vector3 localEuler = cameraTransform.localEulerAngles;
+            localEuler.x = pitch;
+            cameraTransform.localEulerAngles = localEuler;
+        }
     }
 }
