80c42c1f5ae76d7c96a474e178b9a13182e6f04a
ICROSStheLINE <coolwillrule123@gmail.com>
2026-01-09 19:22:58 -0500

    - Crosshair attempt

---
+++ Assets/Crosshair.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    Animator anim;
+    LayerMask designatedPlatformLayerMask;
+    [SerializeField] RectTransform crosshairRectTransform;
+    Vector3 screenCenter;
+    float dogshitAimRadius = 1f;
+    float platformInteractionDistance = 35f;
+    float smoothSpeed = 30f;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        designatedPlatformLayerMask = LayerMask.GetMask("DesignatedPlatform");
+        screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+    }
+
+    void Update()
+    {
+        CheckForDesignatedPlatform();
+    }
+
+    void CheckForDesignatedPlatform()
+    {
+        screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        anim.SetBool("HoveringItem", false);
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+        Vector3 targetPos = screenCenter;
+
+        // Raycast for picking up objects
+        if (Physics.SphereCast(ray, dogshitAimRadius, out hit, platformInteractionDistance, designatedPlatformLayerMask))
+        {
+            targetPos = Camera.main.WorldToScreenPoint(hit.collider.transform.position);
+            anim.SetBool("HoveringItem", true);
+        }
+
+        crosshairRectTransform.position = Vector3.Lerp(
+            crosshairRectTransform.position,
+            targetPos,
+            Time.deltaTime * smoothSpeed
+        );
+    }
+}
